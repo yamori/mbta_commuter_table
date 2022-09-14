f2c5f4f137857e60fd27377b9c1a9a82c6a04f43
Matthew D. Kindzerske <kindzerske@gmail.com>
2022-09-14 18:22:44 +0000

    implemented debugpipe scheme

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 An app/pipeline using the [MBTA v3 API](https://www.mbta.com/developers/v3-api).
 
-The `data_pipe` directory is meant to be a procedural cull of data, eventually piped into a/the format for the FEnd.  Set the API key in `.env` and run `./main.sh` (heavily commented with stages of JSON).  JS scripts are run to make actual API queries and data sorting/mapping/sifting.
+The `data_pipe` directory is meant to be a procedural cull of data, eventually piped into a/the format for the FEnd.  Set the API key (`MBTA_API_KEY="blah"`) in `.env` and run `cd data_pipe; ./main.sh` (a heavily commented bash script with stages of JSON).  JS scripts make actual API queries and data sorting/mapping/sifting.
+
+## Debug
+
+The `data_pipe/main.sh` script is meant to be a pipe with stages of query and processing.
+
+Each `.js` file will be built so it can be executed standalone if the previous step's data is available (JSON file or env var).
+
+The convention is to run the file with the flag `debugpipe`, e.g. `node queryForRoutes.js debugpipe`.
 
 ## Dependencies
 
--- data_pipe/queryForRoutes.js
+++ data_pipe/queryForRoutes.js
@@ -5,38 +5,45 @@ var request = require('request');
 var fs = require('fs'); 
 
 var tempDataDir = process.env.TMP_DATA_DIR;
+var routesJSONFilename = "routes.json";
 
-if (!("STOP_IDS_CSV" in process.env)) {
-   console.log('Environment variable STOP_IDS_CSV is required but was not found');
-   process.exit(1);
+var debugpipe = process.argv.includes('debugpipe') ? true : false;
+
+if (!debugpipe) {
+   queryForRoutesByStops();
+} else {
+   let rawdata = fs.readFileSync(`${tempDataDir}/${routesJSONFilename}`);
+   let routesJSON = JSON.parse(rawdata);
+   outputRouteIDs(routesJSON);
 }
-var stopsListCSV = process.env.STOP_IDS_CSV;
 
-var commuterRailStops = `https://api-v3.mbta.com/routes?filter%5Bstop%5D=${stopsListCSV}&filter%5Btype%5D=2`;
-
-request.get({
-   url: commuterRailStops,
-   json: true,
-   headers: {'User-Agent': 'request', 'x-api-key': process.env.MBTA_API_KEY, 'accept': 'application/vnd.api+json'}
- }, (err, res, data) => {
-   if (err) {
-     console.log('Error:', err);
-   } else if (res.statusCode !== 200) {
-     console.log('Status:', res.statusCode);
-   } else {
-      // data is already parsed as JSON:
-      var routesJSON = data.data;
-      fs.writeFileSync(`${tempDataDir}/routes.json`, JSON.stringify(routesJSON, null, 2) , 'utf-8');
-
-      outputRouteIDs(routesJSON);
+function queryForRoutesByStops() {
+   if (!("STOP_IDS_CSV" in process.env)) {
+      console.log('Environment variable STOP_IDS_CSV is required but was not found');
+      process.exit(1);
    }
-});
-
-
-// // Debug
-// let rawdata = fs.readFileSync(`${tempDataDir}/routes.json`);
-// let routesJSON = JSON.parse(rawdata);
-// outputRouteIDs(routesJSON);
+   var stopsListCSV = process.env.STOP_IDS_CSV;
+   
+   var commuterRailStops = `https://api-v3.mbta.com/routes?filter%5Bstop%5D=${stopsListCSV}&filter%5Btype%5D=2`;
+   
+   request.get({
+      url: commuterRailStops,
+      json: true,
+      headers: {'User-Agent': 'request', 'x-api-key': process.env.MBTA_API_KEY, 'accept': 'application/vnd.api+json'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+         // data is already parsed as JSON:
+         var routesJSON = data.data;
+         fs.writeFileSync(`${tempDataDir}/${routesJSONFilename}`, JSON.stringify(routesJSON, null, 2) , 'utf-8');
+   
+         outputRouteIDs(routesJSON);
+      }
+   });
+}
 
 function outputRouteIDs(routesJSON) {
    var routeIDs = routesJSON.map(a => a.id);
